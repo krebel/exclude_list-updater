0149637e10deb008d9b6fe42d4752e03bd65bffc
krebel <lkrebelweb@gmail.com>
2019-11-09 14:51:01 -0500

    Create mkexcld.sh

---
+++ mkexcld.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#
+###############################
+#
+# mkexcld.sh - automate updating /usr/openv/netbackup/exclude_list entries for anything over 43 days old
+#
+# - Script adds selections to be excluded from backup of filesystem /mnt/remote/gdcz11p00/backup_nbu1 only.
+# - Resulting exclude_list excludes any file with filename containing date string format YYYYMMDD (ex. 20190830) older than 42 days.
+#
+# - Place this script in /usr/openv/netbackup/bin/goodies on the backup client.
+# - Make script executable (chmod 550 /usr/openv/netbackup/bin/goodies/mkexcld.sh)
+# - Schedule execution via cron to run daily.
+# - NOTE: script makes a copy of the original exclude_list in /log/exclude_list<date>
+#
+###############################
+
+# Variables:
+epoch=`date --date="43 days ago" +%Y%m%d`
+yrmon=`echo $epoch | cut -c 1-6`
+endday=`echo $epoch | cut -c 7-8`
+fivemo=`date --date="5 months ago" +%Y%m`
+fourmo=`date --date="4 months ago" +%Y%m`
+threemo=`date --date="3 months ago" +%Y%m`
+
+# main
+
+# keep a copy of the existing exclude_list file:
+cp -p /usr/openv/netbackup/exclude_list /log/exclude_list_`date +%d%b%Y`
+
+# overwrite and add previous 3, 4 and 5 months ago excludes for those entire months:
+echo "/mnt/remote/*/*/*"$fivemo"*" > /usr/openv/netbackup/exclude_list
+echo "/mnt/remote/*/*/*"$fourmo"*" >> /usr/openv/netbackup/exclude_list
+echo "/mnt/remote/*/*/*"$threemo"*" >> /usr/openv/netbackup/exclude_list
+
+
+# For the month the 43rd day ago falls in, create wild card exclude days leading up to
+# the 43rd day ago only if that day is not the 1st of the month:
+
+if [ "$endday" = "01" ]; then
+    echo "/mnt/remote/*/*/*"$yrmon$endday"*" >> /usr/openv/netbackup/exclude_list
+    exit 0
+elif [ "$endday" != "01" ]; then
+    echo "/mnt/remote/*/*/*"$yrmon"[01-"$endday"]*" >> /usr/openv/netbackup/exclude_list
+fi
